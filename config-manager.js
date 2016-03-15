017a13f8334019ae79e5370eb178e6bb32263d1e
istateside <kevin.r.fleischman@gmail.com>
2016-03-15 14:17:25 -0400

    sets up real basic express hello world server

--- config-manager.js
+++
@@ -1,9 +0,0 @@
-app.set('views', PATH.resolve(__dirname, ROOT, nconf.get('templateRoot')));
-
-  app.engine('hbs', HBS({
-      extname:'hbs', 
-      defaultLayout:'main.hbs', 
-      layoutsDir: PATH.resolve(__dirname, ROOT, nconf.get('templateLayouts'))
-  }));
-
-  app.set('view engine', 'hbs');
