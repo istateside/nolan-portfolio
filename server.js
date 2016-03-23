84a1cf655356660a001c508693df6af6bf4bd231
istateside <kevin.r.fleischman@gmail.com>
2016-03-23 00:43:44 -0400

    sets up basic connection to Sequelize and Passport for authentication

--- server.js
+++ server.js
@@ -1,11 +1,69 @@
 const express = require('express');
-const app = express();
+const app     = express();
+
+const cookieParser = require('cookie-parser');
+
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+
+const Sequelize = require('sequelize');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = new Sequelize('aubrey_dev', 'root', '');
+
+sequelize.authenticate().then(
+  function() { console.log("Fuck yeah! db connected."); },
+  function() { console.log("Fuck! db error."); }
+);
 
 app.use(express.static(__dirname + '/public'));
+app.use(session({
+  secret: "thisisasecret",
+  store: new SequelizeStore({db:sequelize}) ,
+}));
+app.use(cookieParser());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    const user = User.find( { where: { username: username, password: password } }).then(function(user) {
+      return done(null, user);
+    }, function() {
+      return done(null, false, { message: "Bad login." });
+    }
+    if (username === "admin" && password === "password") {
+    }
+  }
+));
+
+const User = sequelize.define("User", {
+  username: Sequelize.STRING,
+  password: Sequelize.STRING,
+});
+
+sequelize.sync().then(function() {
+  User
+    .findOrCreate({where: { username: 'admin' }, defaults: { password: 'password'}})
+    .spread(function(user, was_created) {
+      console.log("User instantiated. Found?", was_created, user);
+    });
+});
 
 app.get('/', function(req, res) {
   res.render("index");
 });
+
+app.get('/admin', function(req, res) {
+  res.render("login");
+});
+
+app.post('/admin', passport.authenticate('local', {
+  successRedirect: "/",
+  failureRedirect: "/admin",
+  failureFlash: true
+}););
 
 app.listen(3000, function()  {
   console.log("App listening on port 3000.");
