9a3979afae4c6cfa89052d484c485d0e26dba20e
istateside <kevin.r.fleischman@gmail.com>
2016-06-29 22:27:23 -0400

    fixes syntax errors

--- api-backend/app/models/block.rb
+++ api-backend/app/models/block.rb
@@ -1,7 +1,7 @@
 class Block < ApplicationRecord
   belongs_to :blockable, polymorphic: true
-  has_many :child_blocks, as: :blockable, -> { order(position: :asc) }
-  has_many :images, as: :imageable, -> { order(position: :asc) }
+  has_many :child_blocks, -> { order(position: :asc) }, as: :blockable
+  has_many :images, -> { order(position: :asc) }, as: :imageable
   acts_as_list scope: :blockable
   
   attr_accessor :title, :size, :shape, :text, :position, :is_published
--- api-backend/app/models/page.rb
+++ api-backend/app/models/page.rb
@@ -1,4 +1,5 @@
 class Page < ApplicationRecord
-  has_many :blocks, as: :blockable, -> { order(position: :asc) }
-  has_many :images, as: :imageable, -> { order(position: :asc) }
+  has_many :blocks, -> { order(position: :asc) }, as: :blockable
+  has_many :images, -> { order(position: :asc) }, as: :imageable
+  attr_accessor :name, :presentation, :is_published, :is_in_header
 end
