227cf0746e8c5285dbecef3f390b28a38e5229b4
istateside <kevin.r.fleischman@gmail.com>
2016-09-05 19:02:50 -0400

    Removes devise_token_auth, installs "knock"

--- api-backend/config/routes.rb
+++ api-backend/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     scope :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
       resources :images
       resources :blocks
       resources :pages
