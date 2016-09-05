3d173000825bab77ff4552dfe1225f3961c12ff8
istateside <kevin.r.fleischman@gmail.com>
2016-09-05 19:13:59 -0400

    Sets up basic controllers and installs Knack

--- api-backend/config/routes.rb
+++ api-backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
   namespace :api do
     scope :v1 do
       resources :images
@@ -6,5 +7,4 @@ Rails.application.routes.draw do
       resources :pages
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
